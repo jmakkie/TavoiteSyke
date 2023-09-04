4a2fc1dfc331efc92537be9cfa4b9eefe7f98637
jmakkie <jaapimakkonen13@gmail.com>
2023-09-04 22:27:47 +0300

    fixed calc

--- App.js
+++ App.js
@@ -4,27 +4,29 @@ import {Button, StyleSheet, Text,TextInput, View } from 'react-native';
 import React,{useState} from 'react';
 
 export default function App() {
-  const [age, setAge] = useState("");
-  const [limits, setlimits] = useState("no age set");
+  const [age, setAge] = useState(0);
+  const [loLimits, setLoLimits] = useState(0);
+  const [hiLimits, setHiLimits] = useState(0);
 
-  const beatCalc = (age) => {
+  const beatCalc = () => {
     var upper = (220-age)*0.65;
     var lower = (220-age)*0.85;
 
-    setlimits(upper + "-" + lower);
+    setHiLimits(upper);
+    setLoLimits(lower);
   }
 
   return (
     <View style={styles.container}>
       <Text>age</Text>
       <TextInput 
-        placeholder="no age set" 
-        value={age} 
-        keyboardType='decimal-pad'
-        onChangeText={age => setAge(age)}/>
-      <Button title="calc" onPress={beatCalc}/>
+        placeholder="0" 
+        value={age.toString()} 
+        onChangeText={text => setAge(text)}
+        keyboardType='numeric'/>
       <Text>limits</Text>
-      <Text>{limits}</Text>
+      <Text>{loLimits.toFixed(0)}-{hiLimits.toFixed(0)}</Text>
+      <Button title="calc" onPress={beatCalc}/>
       <StatusBar style="auto" />
     </View>
   );
