3174079e8dbd1dd47272b6499b104950de6c4f82
jmakkie <jaapimakkonen13@gmail.com>
2023-09-04 21:58:28 +0300

    tht made

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text,TextInput, View } from 'react-native';
+//import React from 'react';
+import React,{useState} from 'react';
 
 export default function App() {
+  const [age, setAge] = useState("");
+  const [limits, setlimits] = useState("no age set");
+
+  const beatCalc = (age) => {
+    var upper = (220-age)*0.65;
+    var lower = (220-age)*0.85;
+
+    setlimits(upper + "-" + lower);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>age</Text>
+      <TextInput 
+        placeholder="no age set" 
+        value={age} 
+        keyboardType='decimal-pad'
+        onChangeText={age => setAge(age)}/>
+      <Button title="calc" onPress={beatCalc}/>
+      <Text>limits</Text>
+      <Text>{limits}</Text>
       <StatusBar style="auto" />
     </View>
   );
